51630ce007b2606d5a06f7b33571fabdf91dc8a4
Luis Harvey Triana Vega <harveytriana@hotmail.com>
2024-11-23 15:58:46 -0500

    AuthomatedLog

---
+++ BlazorMinimalSS/Models/AuthomatedLog.cs
@@ -0,0 +1,15 @@
+﻿namespace BlazorMinimalSS.Models;
+
+public class AuthomatedLog(float minValue, float maxValue)
+{
+    readonly Random rand = new(DateTime.Now.Millisecond);
+
+    double seed = 0.0F;
+
+    public float NextValue()
+    {
+        seed = Math.Round(Math.Min(Math.Max(seed + (0.1 - rand.NextDouble() / 5.0), -1), 1), 4);
+        return (maxValue - minValue) * 0.5F * ((float)seed + 1.0F) + minValue;
+    }
+}
+
